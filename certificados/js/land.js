9b4a2227329578d65d62250531d059f0b020b372
Robson Vinicius <robsondgn@outlook.com>
2018-03-27 01:47:36 -0300

    cv e certificados

---
+++ certificados/js/land.js
@@ -0,0 +1,13 @@
+
+  
+  $(".page-scroll").click(function(e){
+        e.preventDefault();
+        var hash = this.hash;
+
+        $("html, body").animate({
+            scrollTop: $(hash).offset().top-73
+        }, 500);   
+    
+    })
+  
+ 
